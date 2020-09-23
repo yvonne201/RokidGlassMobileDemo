eade36b78b3e3ce94fe65cb42a84077aeceed811
何顺 <shunhe1991@gmail.com>
2020-09-23 20:49:59 +0800

    [update] remove test code

--- app/src/test/java/com/rokid/alliancedemo/ExampleUnitTest.java
+++ app/src/test/java/com/rokid/alliancedemo/ExampleUnitTest.java
@@ -12,28 +12,7 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-        A a = new A();
-        B b = new B();
-
-        if (b instanceof A) {
-            System.out.println("b instance b return true");
-        }
-
-        if (b instanceof C) {
-
-        }
-    }
-
-
-    private class A {
 
     }
-
-    private class B extends A implements C{
-
-    }
-
-    private interface C {}
 
 }
